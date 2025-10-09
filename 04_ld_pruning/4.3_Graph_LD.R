bd7ce50dacb7e9c8d9f56fd4f096a2abdc8c3830
eduardo-monge <eduardo.mongee@gmail.com>
2025-10-09 17:25:07 -0300

    Update 4.3_Graph_LD.R

--- 04_ld_pruning/4.3_Graph_LD.R
+++ 04_ld_pruning/4.3_Graph_LD.R
@@ -1,1 +1,21 @@
+library(dplyr)
+library(ggplot2)
 
+r <- read.table("/home/edmonge/wgs_acrocomia/analyses/LD_prunning/LDblocks.ld", header=TRUE, stringsAsFactors=FALSE)
+bin_size <- 1000
+ld_decay <- r %>%
+  mutate(bin = (dist %/% bin_size) * bin_size) %>%
+  group_by(bin) %>%
+  summarise(mean_r2 = mean(r2_ExpG, na.rm = TRUE), .groups = "drop")
+
+p <- ggplot(ld_decay, aes(x = bin, y = mean_r2)) +
+  geom_line(color = "blue", linewidth = 1) +
+  labs(
+    x = "Distance between SNPs (bp)",
+    y = expression(Linkage~disequilibrium~(r^2))) +
+  scale_x_continuous(breaks = seq(0, 100000, by = 10000)) +
+  theme_minimal(base_size = 14)
+
+pdf("/home/edmonge/wgs_acrocomia/R_scripts/plots/LD_decay.pdf", width=8, height=6)
+p
+dev.off()
