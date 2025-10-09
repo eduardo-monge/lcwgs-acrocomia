f71983c0d27af5dd7e125ad7452541bc1dd82828
eduardo-monge <eduardo.mongee@gmail.com>
2025-10-09 15:54:02 -0300

    Update 2.2_quality_filters.sh

--- 02_alignment/2.2_quality_filters.sh
+++ 02_alignment/2.2_quality_filters.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 #SBATCH --job-name=filter_aligment_acrocomia
 #SBATCH --partition=bigmem
-#SBATCH --ntasks-per-node=60
+#SBATCH --ntasks-per-node=100
 #SBATCH --time=168:00:00
 #SBATCH --mem=400gb
-#SBATCH --error=mark_dedup.err
-#SBATCH --output=marl_dedup.out
+#SBATCH --error=filter.err
+#SBATCH --output=filter.out
+
 N=8
 SAMPLES=$(cat /home/edmonge/wgs_acrocomia/samples.txt) #Arquivo de entrada com nome das sequencias
-IN="/home/edmonge/wgs_acrocomia/aligment_filtros_semq"
-OUT="/home/edmonge/wgs_acrocomia/aligment_clip_semq"
-PICARD_PATH="/home/edmonge/wgs_acrocomia/picard.jar"
+SAM="/home/edmonge/wgs_acrocomia/aligment_sem_filtro"
+OUT="/home/edmonge/wgs_acrocomia/aligment_filtros_semq"
 
-echo "INICIADO EM: "$(date) > markduplicates.log
+echo "INICIADO EM: "$(date) > filters.log
 #Para cada amostra, realiza os passos seguinte
 
 i=0
 for sample in $SAMPLES; do
-    echo "CALCULANDO MARKDUPLICATE PARA $sample EM "$(date) >> markduplicates.log
-    ((i=i%N)); ((i++==0)) && wait;
-
-    # Mark duplicated
-    java -jar $PICARD_PATH MarkDuplicates \
-    I=$IN/${sample}.sorted.bam \
-    O=$OUT/${sample}.marked.bam \
-    M=$OUT/${sample}.duprmmetrics.txt \
-    REMOVE_DUPLICATES=true \
-    VALIDATION_STRINGENCY=SILENT &
-done && wait
+        echo "INICIANDO BAM FILTERS PARA $sample EM: "$(date) >> filters.log
+        ((i=i%N)); ((i++==0)) && wait;
+        #Filter and convert to BAM
+        samtools view -b -h -F 4 $SAM/${sample}_mapped.sam > $OUT/$sample.bam &
+done && wait;
 
 i=0
 for sample in $SAMPLES; do
-        echo "INICIANDO CLIPOVERLAP PARA $sample EM: "$(date) >> markduplicates.log
+        echo "CREATING SORTED BAM FOR $sample EM "$(date) >> filters.log
         ((i=i%N)); ((i++==0)) && wait;
 
-        #Do the clip overlap
-        bam clipOverlap \
-        --in $OUT/${sample}.marked.bam \
-        --out $OUT/${sample}.clip.bam \
-        --stats &
-done && wait
+        #Creando sorted e index
+        samtools sort $OUT/$sample.bam -o $OUT/$sample.sorted.bam
+        samtools index $OUT/$sample.sorted.bam &
+done && wait;
+
+i=0
+for sample in $SAMPLES; do
+    echo "CALCULANDO FLAGSTAT PARA $sample EM "$(date) >> filters.log
+    ((i=i%N)); ((i++==0)) && wait;
+
+    # Calcular estadísticas
+    samtools flagstat "$OUT"/${sample}.sorted.bam > "$OUT"/${sample}_flagstat.txt &
+done && wait;
