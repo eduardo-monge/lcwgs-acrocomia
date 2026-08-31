64cb82e20e3c95746e181efefbb3ed807cde69cb
eduardo-monge <eduardo.mongee@gmail.com>
2026-08-31 10:12:14 -0300

    Update 7.3.1_treemix.sh

--- 07_admixture/7.3.1_treemix.sh
+++ 07_admixture/7.3.1_treemix.sh
@@ -5,26 +5,26 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --time=168:00:00
 #SBATCH --mem=100gb
-#SBATCH --error=/home/edmonge/wgs_acrocomia/analyses/TreeMix/03_firstree_bootstrap.log
-#SBATCH --output=/home/edmonge/wgs_acrocomia/analyses/TreeMix/03_firstree_bootstrap.log
+#SBATCH --error=/home/edmonge/wgs_acrocomia/analyses/TreeMix/03_diffent_m.log
+#SBATCH --output=/home/edmonge/wgs_acrocomia/analyses/TreeMix/03_diffent_m.log
 
-LOG="/home/edmonge/wgs_acrocomia/analyses/TreeMix/03_firstree_bootstrap.log"
+LOG="/home/edmonge/wgs_acrocomia/analyses/TreeMix/03_diffent_m.log"
 TREEMIX="/home/edmonge/wgs_acrocomia/programs/Treemix/src/treemix"
 TREEDIR="/home/edmonge/wgs_acrocomia/analyses/TreeMix"
 
 
 for m in {0..10}; do
- for i in {1..10}; do
+ for i in {1..100}; do
  SEED=$RANDOM
  echo "START: m=$m i=$i at $(date)" >> "$LOG"
   $TREEMIX \
   -i "$TREEDIR/Acrocomia.treemix.in.gz" \
-  -o "$TREEDIR/out_m_bootstrap/treemix.${i}.${m}" \
+  -o "$TREEDIR/out_m/treemix.${i}.${m}" \
   -global \
   -seed $SEED \
   -m ${m} \
   -k 500 \
-  -bootstrap \
+  -root Totai \
   -se \
   -noss
   echo "DONE: m=$m i=$i at $(date)" >> "$LOG"
